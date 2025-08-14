34e1ef52ba0a9bc87fa25455364528d249ce9edc
Kalan Jarvis-Loewen <112147975+kalandjl@users.noreply.github.com>
2025-08-14 12:06:16 -0600

    taller video

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import ActionArrow from "@/components/ActionArrow";
 export default function Home() {
     return (    
         <>
-            <main id="home-page" className="h-screen">
-                <section id="hero-section" className="h-screen absolute inset-0 -z-10 overflow-hidden">
-                    <video src="/layout/example_hero_vid.mp4" autoPlay loop muted className="absolute" />
+            <main id="home-page" className="min-h-[150vh]">
+                <section id="hero-section" className="min-h-[150vh] absolute inset-0 -z-10">
+                    <div className="relative w-screen h-[150vh] overflow-x-visible bg-black">
+                        <video src="/layout/example_hero_vid.mp4" autoPlay loop muted className="absolute inset-0 w-full h-full object-cover" />
+                    </div>
                     <div className="absolute inset-0 bg-black opacity-30"></div>
-                    <div className="absolute inset-0 grid place-items-center" id="hero-text-overlay">
+                    <div className="absolute inset-0 grid place-items-center h-screen" id="hero-text-overlay">
                         <div id="hero-text-wrap">
                             <h1 className="agency text-6xl text-white">
                                 SHOT BY KYZEC 
